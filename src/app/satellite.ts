e37d65bfe39cfc9f206e54b424744b391c8094ce
Alexus Rodriguez <arod3837@gmail.com>
2020-04-14 13:10:17 -0500

    commit message

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,2 +1,16 @@
 export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+        // You need to assign the class properties in the constructor.
+        this.name = name;
+        this.type = type;
+        this.launchDate = launchDate;
+        this.orbitType = orbitType;
+        this.operational = operational;
+    }
 }
